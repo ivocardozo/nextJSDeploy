70357093de0be0afdc0ecb49b5b73981762e7f67
icarvar <ivo.cardozo@preservica.com>
2023-01-24 11:45:12 -0400

    Sending http requests to Our API Routes

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -5,7 +5,7 @@ import MeetupDetail from '../../components/meetups/MeetupDetail'
 const MeetupDetails = () => {
   return (
     <MeetupDetail 
-        image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
+        image='tps://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
         title='First Meetup'
         address='Some Street 5, Some City'
         description='This is a first meetup'
--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,22 @@
 // /api/new-meetup
 // POST /api/new/meet-up
+import { MongoClient } from 'mongodb'
+// this code never will run on client side
 
-function handler(req, res) {
+async function handler(req, res) {
     if(req.method === 'POST') {
         const data = req.body
 
-        const {title, image, address, description } = data
+       const client = await MongoClient.connect('mongodb+srv://user:user123@cluster0.7zojtyj.mongodb.net/meetups?retryWrites=true&w=majority')
+       const db = client.db()
+
+       const meetupsCollection = db.collection('meetups')
+       const result = await meetupsCollection.insertOne({data})
+
+       console.log(result)
+
+       client.close()
+       res.status(201).json({message: 'Meetup inserted!'})
     }
 }
 
